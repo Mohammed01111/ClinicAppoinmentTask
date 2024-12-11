e234edcbe598917b59367bf08e04e933433ccfdc
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 10:36:08 +0400

    add ClinicRepo with Constructor Dependency Injection

---
+++ ClinicAppoinmentTask/Repositories/ClinicRepo.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicAppoinmentTask.Repositories
+{
+    public class ClinicRepo
+    {
+        private readonly AppDbContext _context;
+
+        public ClinicRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
