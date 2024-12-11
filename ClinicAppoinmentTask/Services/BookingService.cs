82d9ae220d7b4db6645cefb6d96e3337054abe71
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 12:24:05 +0400

    add BookAppointment method to BookingService class

--- ClinicAppoinmentTask/Services/BookingService.cs
+++ ClinicAppoinmentTask/Services/BookingService.cs
@@ -1,4 +1,5 @@
-﻿using ClinicAppoinmentTask.Repositories;
+﻿using ClinicAppoinmentTask.Model;
+using ClinicAppoinmentTask.Repositories;
 
 namespace ClinicAppoinmentTask.Services
 {
@@ -9,6 +10,21 @@ namespace ClinicAppoinmentTask.Services
         public BookingService(IBookingRepo bookingRepo)
         {
             _bookingRepo = bookingRepo;
+        }
+        public void BookAppointment(int patientId, int clinicId, DateTime date, int slotNumber)
+        {
+            if (slotNumber < 1 || slotNumber > 20)
+                throw new Exception("Slot number must be between 1 and 20");
+
+            var booking = new Booking
+            {
+                PID = patientId,
+                CID = clinicId,
+                Date = date,
+                SlotNumber = slotNumber
+            };
+
+            _bookingRepo.AddBooking(booking);
         }
     }
 }
