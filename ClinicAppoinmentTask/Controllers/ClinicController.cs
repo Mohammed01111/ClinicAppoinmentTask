b8e1025eb95824925c27c403bdd807237f7eb668
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 14:55:40 +0400

    Add methos AddClinic to ClinicController

--- ClinicAppoinmentTask/Controllers/ClinicController.cs
+++ ClinicAppoinmentTask/Controllers/ClinicController.cs
@@ -1,4 +1,5 @@
-﻿using ClinicAppoinmentTask.Services;
+﻿using ClinicAppoinmentTask.Model;
+using ClinicAppoinmentTask.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicAppoinmentTask.Controllers
@@ -12,6 +13,20 @@ namespace ClinicAppoinmentTask.Controllers
         public ClinicController(IClinicService clinicService)
         {
             _clinicService = clinicService;
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddClinic([FromBody] Clinic clinic)
+        {
+            try
+            {
+                _clinicService.AddClinic(clinic);
+                return Ok("Clinic added successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
         }
 
     }
