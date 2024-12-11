67a8f7d3ff8549efb105de775fe33f4c6b7355b4
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 14:54:54 +0400

    add dependency injection to ClinicController

---
+++ ClinicAppoinmentTask/Controllers/ClinicController.cs
@@ -0,0 +1,18 @@
+﻿using ClinicAppoinmentTask.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicAppoinmentTask.Controllers
+{
+    [ApiController]
+    [Route("api/clinic")]
+    public class ClinicController : ControllerBase
+    {
+        private readonly IClinicService _clinicService;
+
+        public ClinicController(IClinicService clinicService)
+        {
+            _clinicService = clinicService;
+        }
+
+    }
+}
