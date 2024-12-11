ed5092ca8647c8a6502d8a8e69cfb4b564dcc645
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 10:04:03 +0400

    add AppDbContext Class

---
+++ ClinicAppoinmentTask/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ClinicAppoinmentTask.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicAppoinmentTask
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Clinic> Clinics { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
