2b7b50053e252a0d0e3b605747dcca9d2a70a00c
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 10:34:34 +0400

    add Constructor Dependency Injection in BookingRepo class

---
+++ ClinicAppoinmentTask/Repositories/BookingRepo.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicAppoinmentTask.Repositories
+{
+    public class BookingRepo
+    {
+        private readonly AppDbContext _context;
+
+        public BookingRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
