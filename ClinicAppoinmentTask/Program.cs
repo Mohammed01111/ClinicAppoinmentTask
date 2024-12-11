6a3910b7a96bdc5116553e87d99c40bb41601bf6
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 10:25:26 +0400

    Add services to the container to program class

--- ClinicAppoinmentTask/Program.cs
+++ ClinicAppoinmentTask/Program.cs
@@ -13,6 +13,7 @@ namespace ClinicAppoinmentTask
             builder.Services.AddDbContext<AppDbContext>(options =>
               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
