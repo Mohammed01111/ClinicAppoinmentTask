ab4794251403a0797575a7cb6a56014d03fd2bd2
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 12:53:48 +0400

    Add UpdateBooking and RemoveBooking to IBookingService

--- ClinicAppoinmentTask/Services/IBookingService.cs
+++ ClinicAppoinmentTask/Services/IBookingService.cs
@@ -8,5 +8,9 @@ namespace ClinicAppoinmentTask.Services
         IEnumerable<Booking> GetAppointmentsByClinic(int clinicId);
         IEnumerable<Booking> GetAppointmentsByName(string name);
         IEnumerable<Booking> GetAppointmentsByPatient(int patientId);
+
+        void UpdateBooking(int bookingId, int patientId, int clinicId, DateTime date, int slotNumber);
+        void RemoveBooking(int bookingId);
+
     }
 }
