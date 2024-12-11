d9aeab0adcc6979757c31b5c71b73acb4a578b3d
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 12:35:42 +0400

    add Constructor Dependency Injection to ClinicService

---
+++ ClinicAppoinmentTask/Services/ClinicService.cs
@@ -0,0 +1,14 @@
+﻿using ClinicAppoinmentTask.Repositories;
+
+namespace ClinicAppoinmentTask.Services
+{
+    public class ClinicService
+    {
+        private readonly IClinicRepo _clinicRepo;
+
+        public ClinicService(IClinicRepo clinicRepo)
+        {
+            _clinicRepo = clinicRepo;
+        }
+    }
+}
