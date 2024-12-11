a070cef3ae3ec4d175fbdf608e39be70aa17e486
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 14:29:27 +0400

    add dependency injection to BookingController

---
+++ ClinicAppoinmentTask/Controllers/BookingController.cs
@@ -0,0 +1,22 @@
+﻿using ClinicAppoinmentTask.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicAppoinmentTask.Controllers
+{
+    [ApiController]
+    [Route("api/booking")]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+        private readonly IPatientService _patientService;
+        private readonly IClinicService _clinicService;
+
+        public BookingController(IBookingService bookingService, IPatientService patientService, IClinicService clinicService)
+        {
+            _bookingService = bookingService;
+            _patientService = patientService;
+            _clinicService = clinicService;
+        }
+
+    }
+}
