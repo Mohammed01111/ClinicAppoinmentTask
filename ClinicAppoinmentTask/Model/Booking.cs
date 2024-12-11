8f7a5399434abe1724b832a0c3978c14f42b6744
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-11 09:46:42 +0400

    add Entity Classes for Database

---
+++ ClinicAppoinmentTask/Model/Booking.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicAppoinmentTask.Model
+{
+    [PrimaryKey(nameof(BookingId), nameof(PID), nameof(CID))]
+    public class Booking
+    {
+        
+        public int BookingId { get; set; }
+
+        [ForeignKey("Patient")]
+        public int PID { get; set; }
+
+        [ForeignKey("Clinic")]
+        public int CID { get; set; }
+        public DateTime Date { get; set; }
+        public int SlotNumber { get; set; } // Ensure validation (1-20)
+
+        public Patient Patient { get; set; }
+        public Clinic Clinic { get; set; }
+    }
+}
